6f7096158ddbbef9612ad80fd08f10937234383e
Joao Patricio <ijpatricio@gmail.com>
2025-04-14 13:56:34 +0100

    Todo test

---
+++ tests/Feature/TodoTest.php
@@ -0,0 +1,10 @@
+<?php
+
+it('cleans up TMP dirs', function () {})->todo("
+    When we create a TMP_dir, we can create a `.meta` file inside
+    it has TmpMeta DTO json encoded
+    It has the creation time, opCode, timeout, etc
+    We can then have a purge function
+");
+
+
